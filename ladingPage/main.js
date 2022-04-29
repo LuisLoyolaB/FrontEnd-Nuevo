8363521bc1eb1a9e1cef0e33f6bf00b07f5b0c95
Luis Loyola <luis.loyola10@inacapmail.cl>
2022-04-29 17:24:25 -0400

    probando las clases en js y jquery, pero no resulto xD

--- ladingPage/main.js
+++ ladingPage/main.js
@@ -26,3 +26,13 @@ function desplegarVideo(){
    })     
     
 }
+$(document).ready(function(){
+    $(window).on("scroll",function(){
+        if($(this).scrollTop() >90){
+            $(".navbar").addClass("navbar-shrink");        
+        }
+        else{
+            $("navbar").removeClass("navbar-shrink");
+        }
+    })
+})
