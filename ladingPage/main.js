ffffb719e3f4ba55dbe4887e5ad4bece2235ae23
Luis Loyola <luis.loyola10@inacapmail.cl>
2022-04-29 16:34:11 -0400

    terminado la seccion del home el inicio y el javascript inicial para la muestra del video

---
+++ ladingPage/main.js
@@ -0,0 +1,28 @@
+window.onload = desplegarVideo()
+// Este codigo esta horrible xdddd 
+// falta mejorarlo
+function desplegarVideo(){
+    const videoSrc = document.getElementById("video-1").setAttribute("src","https://www.youtube.com/embed/Ycs7gq_fRcA")
+    const videoSrc2 = document.getElementById("video-1").getAttribute("src")
+    document.querySelector(".video-play-btn").addEventListener("click",function(){
+            // event.preventDefault()
+        //    alert("hola")
+            if (document.querySelector(".video-popup").classList.contains("open")){
+                document.querySelector(".video-popup").classList.remove("open");
+                // document.getElementById("video-1").setAttribute("src","");
+            }
+            else{
+                document.querySelector(".video-popup").classList.add("open");
+                if(document.querySelector("#video-1").src==""){
+                    document.getElementById("video-1").setAttribute("src",videoSrc);
+                }    
+            }
+        })
+   document.querySelector(".video-popup").addEventListener("click",function(){
+    if (document.querySelector(".video-popup").classList.contains("open")){
+        document.querySelector(".video-popup").classList.remove("open");
+        // document.getElementById("video-1").setAttribute("src","");
+    }
+   })     
+    
+}
